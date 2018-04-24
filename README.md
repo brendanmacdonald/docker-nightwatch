a898add0c2b42fc71c2405f3c978723cf8b2a7bf
Brendan MacDonald <bmacdonald@scottlogic.co.uk>
2018-04-24 09:36:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Tests run on chrome only.
+
+Launch tests with
+
+```
+docker-compose run --rm nightwatch --env dev
+```
+
+Close Docker container with
+```
+docker-compose down
+```
---
+++ test/globals.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  beforeEach: function (browser, done) {
+    // Do stuff before each test, e.g.
+    // require('nightwatch-video-recorder').start(browser, done)
+  },
+  afterEach: function (browser, done) {
+    // Do stuff after each test.
+  }
+}
